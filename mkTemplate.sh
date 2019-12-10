18b3d0d39786572746b2e2e9f4c182ef4fc96009
kkiser <kkiser@ETS-kkiser-MacBook.local>
2019-12-10 09:42:43 -0500

    Shell script to create directory structure for REACT

---
+++ mkTemplate.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+mkdir $1
+cd $1
+cp ../.gitignore .
+
+npm init -y
+npm install react
+npm install react-dom
+
+npm install --save-dev @babel/core @babel/preset-env @babel/preset-react webpack webpack-cli webpack-dev-server babel-loader css-loader style-loader html-webpack-plugin 
+
+npm install webpack webpack-cli
+npm install babel-loader
+
+cp ../Challange5/webpack.config.js .
+
+mkdir app
+cp ../index.js app/
+cp ../index.html app/
+cp ../index.css app/
+
+
+
+
